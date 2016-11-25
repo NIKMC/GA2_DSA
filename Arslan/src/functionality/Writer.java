115bfaaf112c2fcb08139663bd615f7773f5c418
NIKMC <nikmc10@mail.ru>
2016-11-25 13:34:09 +0300

    added some models

---
+++ Arslan/src/functionality/Writer.java
@@ -0,0 +1,7 @@
+package functionality;
+
+/**
+ * Created by NIKMC on 25.11.16.
+ */
+public class Writer {
+}
---
+++ Arslan/src/model/RocketLauncherUltimateNitroTurboBoostSuperSpace3000.java
@@ -0,0 +1,22 @@
+package model;
+
+import java.awt.*;
+
+/**
+ * Created by NIKMC on 25.11.16.
+ */
+public class RocketLauncherUltimateNitroTurboBoostSuperSpace3000 {
+    private Point location;
+
+    public RocketLauncherUltimateNitroTurboBoostSuperSpace3000(Point location) {
+        this.location = location;
+    }
+
+    public Point getLocation() {
+        return location;
+    }
+
+    public void setLocation(Point location) {
+        this.location = location;
+    }
+}
