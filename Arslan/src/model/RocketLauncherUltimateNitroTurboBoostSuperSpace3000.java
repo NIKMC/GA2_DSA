65105b1113361ac7a032f30938753e999e6cce4d
Stanislav <nikstas94@mail.ru>
2016-11-25 16:11:23 +0300

    Angle Checker

--- Arslan/src/model/RocketLauncherUltimateNitroTurboBoostSuperSpace3000.java
+++ Arslan/src/model/RocketLauncherUltimateNitroTurboBoostSuperSpace3000.java
@@ -1,15 +1,20 @@
 package model;
 
 import java.awt.*;
+import java.util.LinkedList;
 
 /**
  * Created by NIKMC on 25.11.16.
  */
 public class RocketLauncherUltimateNitroTurboBoostSuperSpace3000 {
     private Point location;
+    private Double attackAngle;
+    private LinkedList<MyRectangle> underStrike;
 
-    public RocketLauncherUltimateNitroTurboBoostSuperSpace3000(Point location) {
+    public RocketLauncherUltimateNitroTurboBoostSuperSpace3000(Point location, Double attackAngle) {
         this.location = location;
+        this.attackAngle = attackAngle;
+        this.underStrike = new LinkedList<MyRectangle>();
     }
 
     public Point getLocation() {
@@ -18,5 +23,21 @@ public class RocketLauncherUltimateNitroTurboBoostSuperSpace3000 {
 
     public void setLocation(Point location) {
         this.location = location;
+    }
+
+    public Double getAttackAngle() {
+        return attackAngle;
+    }
+
+    public void setAttackAngle(Double attackAngle) {
+        this.attackAngle = attackAngle;
+    }
+
+    public LinkedList<MyRectangle> getUnderStrike() {
+        return underStrike;
+    }
+
+    public void setUnderStrike(LinkedList<MyRectangle> underStrike) {
+        this.underStrike = underStrike;
     }
 }
