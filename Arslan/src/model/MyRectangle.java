2a41bab65285c8c02f226122d7850c8fbbb40ab1
NIKMC <nikmc10@mail.ru>
2016-11-25 15:07:30 +0300

    Completed reader

--- Arslan/src/model/MyRectangle.java
+++ Arslan/src/model/MyRectangle.java
@@ -10,7 +10,7 @@ public class MyRectangle {
     private int height;
     private int width;
 
-    public MyRectangle(Point leftTop, int height, int width) {
+    public MyRectangle(Point leftTop, int width, int height) {
         this.leftTop = leftTop;
         this.height = height;
         this.width = width;
